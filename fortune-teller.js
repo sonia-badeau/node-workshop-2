f5240ce9b3620cd6e5733479a581277cc7c8be7d
Sonia <sonia.badeau@gmail.com>
2016-02-02 23:16:02 +0000

    I output the number of fortunes requested by the usered

--- fortune-teller.js
+++ fortune-teller.js
@@ -1,4 +1,5 @@
 var fortune = require("./library/fortune.js"); //fait appel à fortune.js
-    
-console.log(fortune());
-
+/*console.log("This will loop " + process.argv[2] + " times.");*/
+for (var i = 0; i < process.argv[2]; i++) {
+  console.log(fortune());  
+}
