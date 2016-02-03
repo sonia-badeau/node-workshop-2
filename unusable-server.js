3c07b3fea40f83972a4c8499656ab0323c639777
Sonia <sonia.badeau@gmail.com>
2016-02-03 00:16:05 +0000

    unusable-server.js

---
+++ unusable-server.js
@@ -0,0 +1,11 @@
+var http = require("http");
+
+var server = http.createServer(function(request, response) {
+    var start = new Date().getTime();
+    while (new Date().getTime() - start < 10000);
+    
+    response.end("hello world!"); 
+
+});
+
+server.listen(process.env.PORT); 
